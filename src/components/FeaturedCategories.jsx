500fdae4c4a76f73e1fe521ae4baf8fe28487ee5
Zaryab Awan <zariabawan6@gmail.com>
2026-07-28 02:43:54 +0500

    Use local sareee.webp image for Sarees category card

--- src/components/FeaturedCategories.jsx
+++ src/components/FeaturedCategories.jsx
@@ -29,8 +29,7 @@ const FeaturedCategories = () => {
       title: "Sarees",
       description: "Timeless drapes, elegantly woven.",
       // Traditional saree editorial shot
-      image:
-        "https://images.unsplash.com/photo-1769500804057-ca1391bf4617?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "/sareee.webp",
       link: "/shop?category=Sarees",
       className: "col-span-12 md:col-span-4",
     },
